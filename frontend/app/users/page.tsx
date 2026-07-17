634329c14c1729685a0f233b0cb0f128f5009d49
haowei <212410061@0365.tku.edu.tw>
2026-07-17 19:32:20 +0800

    feat(user):add user button

--- frontend/app/users/page.tsx
+++ frontend/app/users/page.tsx
@@ -1,6 +1,8 @@
 import NavigationBar from '@/components/ui/NavigationBar'
 import { navigationBarList } from '../dashboard/page'
 import { Header } from '@/components/Header'
+import { CgExport } from 'react-icons/cg'
+import { FaPlus } from 'react-icons/fa'
 export default function Users() {
   return (
     <div className='w-full h-full flex'>
@@ -15,6 +17,17 @@ export default function Users() {
             <span className='text-gray-400 text-sm font-bold'>
               Manage all system users.
             </span>
+          </div>
+
+          <div className='buttons w-[80%] flex justify-end gap-2 mt-20'>
+            <button className='flex justify-center items-center w-25 h-9 border border-gray-200 rounded-sm bg-white gap-2 font-bold'>
+              <CgExport />
+              Export
+            </button>
+            <button className='flex justify-center items-center w-30 h-9 border border-gray-200 rounded-sm bg-blue-500 font-bold text-white gap-2'>
+              <FaPlus />
+              add User
+            </button>
           </div>
         </div>
       </div>
