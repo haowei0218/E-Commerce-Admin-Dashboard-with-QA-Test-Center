a2991f8dcfad853cd048d8a0ab279cfa9dc0f5f0
Haowei <212410061@o365.tku.edu.tw>
2026-07-22 16:43:50 +0800

    feat(user):/api/users/getUserByProperties

--- Backend/src/schema/query/users.query.schema.ts
+++ Backend/src/schema/query/users.query.schema.ts
@@ -1,11 +1,13 @@
 import { gql } from "graphql-tag";
-import { UserLoginResponse, GetUsersResponse,UserResponse } from "../type/users.type.schema.js";
+import { UserLoginResponse, GetUsersResponse } from "../type/users.type.schema.js";
 export const UserQueryDefs = gql`
     ${UserLoginResponse}
     ${GetUsersResponse}
     type Query {
         UserLogin(account:String,password:String):UserLoginResponse!
         GetUsers:GetUsersResponse!
-        GetUserById(userId:String):UserResponse!
+        GetUserById(userId:String):GetUsersResponse!
+        GetUserByProperties(status:String,role_id:Int,keyword:String):GetUsersResponse!
+        
     }
 `;
--- Backend/src/type/user.query.type.ts
+++ Backend/src/type/user.query.type.ts
@@ -1,5 +1,8 @@
 import { user } from "./user.base.type.js"
 
+export type roleCode = 'Admin' | 'Manager' | 'Staff' | 'Viewer' | 'QA' | 'Developer'
+export type accountStatus = 'Inactive' | "Active"
+
 export type userLoginPayload = {
     account: string
     password: string
@@ -11,9 +14,28 @@ export type UserLoginResponse = {
 }
 
 export type GetUsersResponse = {
-    getUsers: user[]
+    getUsers: queryUser[]
 }
 
 export type GetUserByIdResponse = {
-    userInfo: user
+    getUsers: queryUser
+}
+
+export type getUserByPropertiesPayload = {
+    status: accountStatus
+    role_id: number
+    keyword: string
+}
+
+export type queryUser = {
+    id: string
+    name: string
+    email: string
+    code: roleCode
+    status: string
+    create_at: string
+}
+
+export type GetUserByPropertiesResponse = {
+    getUsers: queryUser[]
 }
