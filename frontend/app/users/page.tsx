f3a931cbd7da42dbb3def65ca18716bece45ddc5
haowei <212410061@0365.tku.edu.tw>
2026-07-30 22:21:05 +0800

    更新套件

--- frontend/app/users/page.tsx
+++ frontend/app/users/page.tsx
@@ -11,20 +11,20 @@ import { HiOutlineDotsHorizontal } from 'react-icons/hi'
 import { adminUserProfile } from '@/type/adminUser.type'
 import { getAdminUserByProperties, getUsers } from '@/lib/user.api'
 import { Ring } from '@/components/ring'
-import { CiFilter } from "react-icons/ci";
+import { CiFilter } from 'react-icons/ci'
 import { useRouter } from 'next/navigation'
-import { CiEdit } from "react-icons/ci";
+import { CiEdit } from 'react-icons/ci'
 import PageTitle from '@/components/ui/PageTitle'
 import Link from 'next/link'
-import { MdBlock } from "react-icons/md";
+import { MdBlock } from 'react-icons/md'
 const Roles = [
-  { value: "6", optionName: '工程師' },
-  { value: "5", optionName: '測試人員' },
-  { value: "4", optionName: '唯讀帳號' },
-  { value: "3", optionName: '客服/營運人員' },
-  { value: "2", optionName: '營運主管' },
-  { value: "1", optionName: '管理員' },
-  { value: "All", optionName: 'All' },
+  { value: '6', optionName: '工程師' },
+  { value: '5', optionName: '測試人員' },
+  { value: '4', optionName: '唯讀帳號' },
+  { value: '3', optionName: '客服/營運人員' },
+  { value: '2', optionName: '營運主管' },
+  { value: '1', optionName: '管理員' },
+  { value: 'All', optionName: 'All' },
 ]
 
 const Status = [
@@ -42,9 +42,6 @@ const Status = [
   },
 ]
 
-
-
-
 export function formatDate(value: string | number | Date) {
   return new Intl.DateTimeFormat('zh-TW', {
     year: 'numeric',
@@ -54,23 +51,22 @@ export function formatDate(value: string | number | Date) {
 }
 
 export default function Users() {
-  const [role, setRole] = useState<string>("All")
+  const [role, setRole] = useState<string>('All')
   const [userStatus, setUserStatus] = useState<string>('All')
   const [keywords, setKeywords] = useState<string>('')
   const [Users, setUsers] = useState<adminUserProfile[] | null>(null)
   const router = useRouter()
 
-
   async function handleAdminUserFilter(type: 'Filter' | 'Search') {
     const variables = {
       keyword: type === 'Filter' ? null : keywords.trim(),
       roleId: type === 'Search' ? null : role === 'All' ? null : Number(role),
-      status: type === 'Search' ? null : userStatus === 'All' ? null : userStatus
+      status:
+        type === 'Search' ? null : userStatus === 'All' ? null : userStatus,
     }
     const result = await getAdminUserByProperties(variables)
     if (result.GetAdminUserByProperties.getUsers) {
       setUsers(result.GetAdminUserByProperties.getUsers)
-
     }
   }
 
@@ -118,17 +114,25 @@ export default function Users() {
       <div className='main w-full flex flex-col h-full'>
         <Header />
         <div className='bg-gray-50 w-full h-full p-5'>
-          <PageTitle children={
-            <div className='flex gap-2'>
-              <button className='flex justify-center items-center w-25 h-9 border border-gray-300 rounded-lg bg-white gap-2 font-bold'>
-                <CgExport />
-                Export
-              </button>
-              <Link className='flex justify-center items-center w-30 h-9 border border-gray-300 rounded-lg bg-blue-500 font-bold text-white gap-2 hover:bg-blue-700 hover:cursor-pointer' href="/users/add-user">
-                <FaPlus />
-                add User
-              </Link>
-            </div>} mainTitle='Users' subTitle='Manage all system users.' />
+          <PageTitle
+            children={
+              <div className='flex gap-2'>
+                <button className='flex justify-center items-center w-25 h-9 border border-gray-300 rounded-lg bg-white gap-2 font-bold'>
+                  <CgExport />
+                  Export
+                </button>
+                <Link
+                  className='flex justify-center items-center w-30 h-9 border border-gray-300 rounded-lg bg-blue-500 font-bold text-white gap-2 hover:bg-blue-700 hover:cursor-pointer'
+                  href='/users/add-user'
+                >
+                  <FaPlus />
+                  add User
+                </Link>
+              </div>
+            }
+            mainTitle='Users'
+            subTitle='Manage all system users.'
+          />
           {/* filter*/}
           <div className='filters min-w-[60%] flex justify-start items-center gap-2 mt-10'>
             <div className='flex min-w-85 h-9'>
@@ -138,7 +142,10 @@ export default function Users() {
                 onChange={(e) => setKeywords(e.target.value)}
                 value={keywords}
               />
-              <button className='border-t border-r border-b bg-white border-gray-300 rounded-r-lg pr-2' onClick={() => handleAdminUserFilter("Search")}>
+              <button
+                className='border-t border-r border-b bg-white border-gray-300 rounded-r-lg pr-2'
+                onClick={() => handleAdminUserFilter('Search')}
+              >
                 <CiSearch className='text-2xl' />
               </button>
             </div>
@@ -162,10 +169,16 @@ export default function Users() {
             </div>
 
             <div className='flex gap-2'>
-              <button className='flex justify-center items-center w-25 h-9 border border-gray-300 rounded-lg bg-white gap-2 font-bold hover:bg-gray-500 hover:text-white hover:cursor-pointer' onClick={resetFilter}>
+              <button
+                className='flex justify-center items-center w-25 h-9 border border-gray-300 rounded-lg bg-white gap-2 font-bold hover:bg-gray-500 hover:text-white hover:cursor-pointer'
+                onClick={resetFilter}
+              >
                 Clear
               </button>
-              <button className='flex justify-center items-center w-30 h-9 border border-gray-300 rounded-lg bg-white gap-2 font-bold hover:bg-gray-500 hover:text-white hover:cursor-pointer' onClick={() => handleAdminUserFilter("Filter")}>
+              <button
+                className='flex justify-center items-center w-30 h-9 border border-gray-300 rounded-lg bg-white gap-2 font-bold hover:bg-gray-500 hover:text-white hover:cursor-pointer'
+                onClick={() => handleAdminUserFilter('Filter')}
+              >
                 <CiFilter />
                 Filter
               </button>
@@ -224,8 +237,9 @@ export default function Users() {
                           className={`text-left text-sm font-medium w-30 font-stretch-condensed block  h-[90%]`}
                         >
                           <div
-                            className={`w-15 h-6 flex justify-center items-center ${statusStyle[user.status]
-                              } rounded-md`}
+                            className={`w-15 h-6 flex justify-center items-center ${
+                              statusStyle[user.status]
+                            } rounded-md`}
                           >
                             <span>{user.status}</span>
                           </div>
@@ -234,12 +248,13 @@ export default function Users() {
                           {formatDate(user.create_at)}
                         </th>
                         <th className='flex items-start gap-2 text-left text-sm font-black font-stretch-condensed mb-2'>
-                          <button className='flex items-center gap-2 text-xl font-black border border-gray-200 rounded-sm  hover:bg-gray-500 bg:text-white hover:cursor-pointer' onClick={()=> router.push('/users/edit-user')}>
-                            <span className='text-xs font-bold'>Edit</span>
+                          <button
+                            className='flex items-center gap-2 text-xl font-black border border-gray-200 rounded-sm  hover:bg-gray-500 bg:text-white hover:cursor-pointer'
+                            onClick={() => router.push('/users/edit-user')}
+                          >
                             <CiEdit className='m-auto' />
                           </button>
-                          <button className='flex items-center gap-2 text-xl font-black border border-gray-200 rounded-sm  hover:bg-gray-500 bg:text-white hover:cursor-pointer' >
-                            <span className='text-xs font-bold'>block</span>
+                          <button className='flex items-center gap-2 text-xl font-black border border-gray-200 rounded-sm  hover:bg-gray-500 bg:text-white hover:cursor-pointer'>
                             <MdBlock className='m-auto' />
                           </button>
                         </th>
